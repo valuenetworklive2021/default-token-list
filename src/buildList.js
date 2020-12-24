d9a25ca21640e46d33e8620babe9d8eb2c8614a1
Aleksandr Ivannikov <a.n.ivannikov@yandex.ru>
2020-12-24 08:30:32 +0300

    v1.0.1

--- src/buildList.js
+++ src/buildList.js
@@ -8,7 +8,7 @@ const kovan = require('./tokens/kovan.json');
 module.exports = function buildList() {
   const parsed = version.split('.');
   return {
-    'name': 'Valueswap Default List',
+    'name': 'Valueswap Token List',
     'timestamp': (new Date().toISOString()),
     'version': {
       'major': +parsed[ 0 ],
